d99368a24dc2c92cd5eaaa3db7b181653d0c017a
paulwallis42 <paulwallis42@gmail.com>
2015-07-21 08:10:58 +0100

    game board view on web

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -2,18 +2,32 @@ require 'sinatra/base'
 require 'battleships'
 
 class BattleshipWeb < Sinatra::Base
+  attr_reader :game
+  attr_reader :coordinate
+  attr_reader :orientation
+  attr_reader :ship
+
   get '/' do
     erb :index
   end
 
   get '/new_game' do
     @name = params[:name]
+    @ship = params[:ship]
+    @coordinate = params[:coordinate]
+    @orientation = params[:orientation]
     erb :new_game
   end
 
   get '/start_game' do
     @game = Game.new Player, Board
+    game.player_1.place_ship Ship.battleship, :B4, :vertically
     erb :start_game
+  end
+
+  get '/own_board' do
+    @p1_board = game.own_board_view game.player_1
+    erb :own_board
   end
 
   run! if app_file == $0
